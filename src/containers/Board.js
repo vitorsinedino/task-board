9daf3ccfab597a8ebc057cf5c9ec136b0376dfa6
vitorsinedino <vitorsinedino@gmail.com>
2020-05-13 17:53:28 -0300

    page default composition

---
+++ src/containers/Board.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import Lane from "../components/Lane/Lane";
+
+const BoardWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  margin: 5%;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      loading: true,
+      error: "",
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const tickets = await fetch("../../assets/data.json");
+      const ticketsJSON = await tickets.json();
+
+      if (ticketsJSON) {
+        this.setState({ loading: false, data: ticketsJSON });
+      }
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: error.message,
+      });
+    }
+  }
+
+  render() {
+    const lanes = [
+      { id: 1, title: "To Do" },
+      { id: 2, title: "In Progress" },
+      { id: 3, title: "Review" },
+      { id: 4, title: "Done" },
+    ];
+
+    return (
+      <BoardWrapper>
+        {lanes.map((lane) => (
+          <Lane key={lane.id} title={lane.title} />
+        ))}
+      </BoardWrapper>
+    );
+  }
+}
+
+export default Board;
